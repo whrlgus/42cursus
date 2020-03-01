a337dc62c4f850aafb40378573ceaf7ed4eb7a90
gihyun <whrlgus33@gmail.com>
2020-03-01 22:05:39 +0900

    del

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -20,24 +20,3 @@ t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
 	
 	return (ret);
 }
-
-//t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-//{
-//	t_list	*newnode;
-//	t_list	*newcontent;
-//​
-//	if (!f || !del)
-//		return (NULL);
-//	newnode = NULL;
-//	while (lst)
-//	{
-//		if (!(newcontent = ft_lstnew(f(lst->content))))
-//		{
-//			ft_lstclear(&lst, del);
-//			return (NULL);
-//		}
-//		ft_lstadd_back(&newnode, newcontent);
-//		lst = lst->next;
-//	}
-//	return (newnode);
-//}
