8f3fd650731800caa995fe2ba803103bddc957a3
gihyun <whrlgus33@gmail.com>
2020-03-14 14:39:53 +0900

    yet

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,86 +1,23 @@
 #include "get_next_line.h"
-#define BUFFER_SIZE 1
 
-
-size_t	ft_strlen(const char *s)
+int read_buf(t_buffer *buffer, char **line)
 {
-	size_t ret;
+	size_t	i;
+	size_t	j;
+	int		found;
 	
-	ret = 0;
-	while (*s++)
-		++ret;
-	return (ret);
-}
-
-t_buffer *new_buffer(int fd)
-{
-	t_buffer *ret;
-	
-	if(!(ret = malloc(sizeof(t_buffer))))
-		return 0;
-	ret->fd = fd;
-	ret->next = 0;
-	if(!(ret->buf = malloc(BUFFER_SIZE + 1)))
-		return 0;
-	ret->buf[0]=0;
-	return ret;
-}
-
-t_buffer *find_buffer(t_buffer *buffer, int fd)
-{
-	if(buffer->fd == fd)
-		return buffer;
-	if(!buffer->next)
-		return buffer->next = new_buffer(fd);
-	return find_buffer(buffer->next, fd);
-}
-
-int resize(char **line, size_t len)
-{
-	char *new_line;
-	size_t i;
-	
-	if(!(new_line = malloc(len + BUFFER_SIZE + 1)))
-		return 0;
-	i = 0;
-	while (i < len)
-	{
-		new_line[i] = (*line)[i];
-		++i;
-	}
-	free(*line);
-	*line = new_line;
-	return 1;
-}
-
-int read_buf(t_buffer *buffer, char **line, int *eof)
-{
-	size_t i;
-	size_t j;
-	int ret;
-	
-	
-	
-	if (*line){
-		i = ft_strlen(*line);
-		if(!resize(line, i))
-			return -1;
-	}
-	else
-	{
-		i = 0;
-		*line = malloc(BUFFER_SIZE + 1);
-	}
-	ret = 0;
+	i = ft_strlen(*line);
+	if(!resize(line, i))
+		return (-1);
+	found = 0;
 	j = 0;
 	while (buffer->buf[j])
 	{
 		(*line)[i++] = buffer->buf[j];
 		if (buffer->buf[j++] == '\n')
 		{
-			*eof = 0;
 			--i;
-			ret = 1;
+			found = 1;
 			break;
 		}
 	}
@@ -89,39 +26,27 @@ int read_buf(t_buffer *buffer, char **line, int *eof)
 	while (buffer->buf[j])
 		buffer->buf[i++] = buffer->buf[j++];
 	buffer->buf[i] = 0;
-	return ret;
+	return (found);
 }
 
 int get_next_line(int fd, char **line)
 {
-	static t_buffer *head;
-	t_buffer *buffer;
-	ssize_t read_size;
-	int found;
-	int eof;
+	static t_buffer	*head;
+	t_buffer		*buffer;
+	int				size;
+	int				found;
 	
-	if (BUFFER_SIZE <= 0 || !line)
-	return -1;
-	
-	eof = 1;
-
+	if (BUFFER_SIZE <= 0 || !line
+		|| (!head && !(head = new_buffer(fd)))
+		|| !(buffer = find_buffer(head, fd)))
+		return (-1);
 	*line = 0;
-	if((!head && !(head = new_buffer(fd))) || !(buffer = find_buffer(head, fd))
-	   || (found = read_buf(buffer, line, &eof)) == -1)
-		return -1;
-	
-	if (found)
-		return 1;
-	
-	
-	while((read_size = read(fd,buffer->buf,BUFFER_SIZE))){
-		if(read_size == -1)
-			return -1;
-		buffer->buf[read_size] = 0;
-		if((found = read_buf(buffer, line, &eof)) == -1)
-			return -1;
-		if(found)
-			return 1;
+	if ((found = read_buf(buffer, line)))
+		return (found);
+	while((size = read(fd,buffer->buf,BUFFER_SIZE))>0){
+		buffer->buf[size] = 0;
+		if ((found = read_buf(buffer, line)))
+			return (found);
 	}
-	return !eof;
+	return (size);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,6 +1,8 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+//#define BUFFER_SIZE 1
+
 # include<stdlib.h>
 # include<unistd.h>
 
@@ -11,6 +13,11 @@ typedef struct s_buffer
 	struct s_buffer *next;
 }t_buffer;
 
+
+size_t	ft_strlen(const char *s);
+t_buffer *new_buffer(int fd);
+t_buffer *find_buffer(t_buffer *buffer, int fd);
+int resize(char **line, size_t len);
 int get_next_line(int fd, char **line);
 
 #endif
