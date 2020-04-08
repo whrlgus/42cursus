93ecdd26f52714f44edb503b2f8e535a2c6a453b
gihyun <whrlgus33@gmail.com>
2020-04-08 14:39:01 +0900

    norm

--- Libft/libft.h
+++ Libft/libft.h
@@ -18,8 +18,8 @@
 
 typedef struct		s_list
 {
-    void			*content;
-    struct s_list	*next;
+	void			*content;
+	struct s_list	*next;
 }					t_list;
 
 void		*ft_memset(void *b, int c, size_t n);
@@ -66,6 +66,6 @@ void		ft_lstadd_back(t_list **lst, t_list *new);
 void		ft_lstdelone(t_list *lst, void (*del)(void*));
 void		ft_lstclear(t_list **lst, void (*del)(void*));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
-t_list		*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
