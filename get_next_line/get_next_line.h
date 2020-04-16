7ee8404d42c7cf7ff24e693e6fca91530c116aca
gihyun <whrlgus33@gmail.com>
2020-04-16 22:08:47 +0900

    go

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -23,9 +23,9 @@ typedef struct		s_buffer
 	struct s_buffer	*next;
 }					t_buffer;
 
-size_t			ft_strlen(const char *s);
-t_buffer		*new_buffer(int fd);
-t_buffer		*find_buffer(t_buffer *buffer, int fd);
-int				resize(char **line, size_t len);
-int				get_next_line(int fd, char **line);
+size_t				ft_strlen(const char *s);
+t_buffer			*new_buffer(int fd);
+t_buffer			*find_buffer(t_buffer *buffer, int fd);
+int					resize(char **line, size_t len);
+int					get_next_line(int fd, char **line);
 #endif
