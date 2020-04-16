8378c60d663d00786e4c26b8f6f7b1183751624f
gihyun <whrlgus33@gmail.com>
2020-04-16 22:06:10 +0900

    go

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -13,23 +13,19 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-//#define BUFFER_SIZE 1
-
 # include<stdlib.h>
 # include<unistd.h>
 
-typedef struct s_buffer
+typedef struct	s_buffer
 {
-	int fd;
-	char *buf;
-	struct s_buffer *next;
-}t_buffer;
+	int			fd;
+	char		*buf;
+	struct		s_buffer *next;
+}				t_buffer;
 
-
-size_t	ft_strlen(const char *s);
-t_buffer *new_buffer(int fd);
-t_buffer *find_buffer(t_buffer *buffer, int fd);
-int resize(char **line, size_t len);
-int get_next_line(int fd, char **line);
-
+size_t			ft_strlen(const char *s);
+t_buffer		*new_buffer(int fd);
+t_buffer		*find_buffer(t_buffer *buffer, int fd);
+int				resize(char **line, size_t len);
+int				get_next_line(int fd, char **line);
 #endif
